1acac8c5b7285f3de2ae1768f150b04c253f4935
Vitor Guedes <vgm_rox@hotmail.com>
2022-08-02 17:31:34 -0300

    Add math operations for calculator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
 import Calculator from './components/Calculator';
 import Navigation from './components/Navigation';
 
-function App() {
-  return (
-    <div>
-      <header><Navigation /></header>
-      <main className="main-container">
-        <Calculator />
-      </main>
-      <footer className="footer-container" />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header><Navigation /></header>
+        <main className="main-container">
+          <Calculator />
+        </main>
+        <footer className="footer-container" />
+      </div>
+    );
+  }
 }
 
 export default App;
