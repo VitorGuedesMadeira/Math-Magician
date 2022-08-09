fc11a2b2736658a5c96ae13ed47a8acf7b91bb90
Vitor Guedes <vgm_rox@hotmail.com>
2022-08-09 19:47:59 -0300

    add hamburger icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
-const Navbar = () => (
-  <nav className="navigation">
-    <span>Math Magician</span>
-    <ul>
-      <li>
-        <Link to="/" className="links">Home</Link>
-      </li>
-      <li>
-        <Link to="/Calculator" className="links">Calculator</Link>
-      </li>
-      <li>
-        <Link to="/Quote" className="links">Quote</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
+  return (
+    <nav className="navigation navBar">
+      <span>Math Magician</span>
+      <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
+        <li>
+          <NavLink
+            to="/"
+            activeClassName="active-link"
+            className="links"
+            onClick={() => closeMenu()}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="Calculator"
+            activeClassName="active-link"
+            className="links"
+            onClick={() => closeMenu()}
+          >
+            Calculator
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="Quote"
+            activeClassName="active-link"
+            className="links"
+            onClick={() => closeMenu()}
+          >
+            Quote
+          </NavLink>
+        </li>
+      </ul>
+      <button type="button" onClick={handleToggle}>
+        {navbarOpen ? (
+          <MdClose style={{ color: '#fff', width: '40px', height: '40px' }} />
+        ) : (
+          <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+        )}
+      </button>
+    </nav>
+  );
+};
 export default Navbar;
